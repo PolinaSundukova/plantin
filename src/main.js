6e024f9b8baf51c7ff0b92b75df7a6ef61d82060
polina.sundukova <polina.sundukova@student.howest.be>
2025-01-23 22:29:55 +0100

    added scrolling transitions, added Lottie animation, solved positioning issues

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import './reset.css'
 import './style.css'
+import { DotLottie } from '@lottiefiles/dotlottie-web';
+
+new DotLottie({
+  autoplay: true,
+  loop: true,
+  // mode: "reverse",
+  // marker: "box", // box, ball, hexa
+  canvas: document.querySelector("#anim-box"),
+  src: "./assets/engraving.json",
+});
+
+
+
+
 
 
 const links = document.querySelectorAll('.navigation li a');
@@ -18,7 +32,7 @@ links.forEach(link => {
   });
 });
 
-
+// const hamburgerMenu = document.querySelector('.hamburger_menu')
 const hamburgerBtn = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.menu_navigation');
 const hamburgerIcon = document.querySelector('.hamburger-icon');
@@ -61,7 +75,7 @@ navLinks.forEach(link => {
 
 
 
-//pin - for two-halfs img to be stacked while the text is appearing between them
+
 
 const timelineScrollAnimation = () => {
   const timecodes = gsap.utils.toArray(".timeline .timecode");
@@ -135,18 +149,7 @@ const introAnimation = () => {
       x: '45vw',
       opacity: 0,
     }, "<") // trigger second at the
-  //
 
-  // .to([".intro_gravure--1", ".intro_gravure--2"], {
-  //   x: -100,
-  // })
-
-  // .to([
-  //   { targets: ".intro_gravure--1", x: -100 },
-  //   { targets: ".intro_gravure--2", x: 100 }
-  // ], {
-  //   duration: 1,
-  // });
 
   ScrollTrigger.create({
     trigger: ".intro_gravures",
@@ -250,10 +253,157 @@ const transitionAnimation = () => {
   });
 }
 
+
+
+
+const typographyAnimation = () => {
+
+  // const $intro = document.querySelector(".intro")
+  const $garamondGravures = document.querySelector(".garamond_gravures")
+  const garamondGravure = gsap.timeline()
+
+    .to(".garamond_gravure--1", {
+      x: '-45vw',
+      opacity: 0,
+    })
+    .to(".garamond_gravure--2", {
+      x: '45vw',
+      opacity: 0,
+    }, "<") // trigger second at the
+
+
+  ScrollTrigger.create({
+    trigger: ".garamond_gravures",
+    animation: garamondGravure,
+    start: "top 20%",
+    end: "bottom 0%",
+    scrub: 1,
+    pin: $garamondGravures,
+    // pin: $intro,
+    // markers: true
+  });
+}
+
+const typographySecondAnimation = () => {
+
+  // const $intro = document.querySelector(".intro")
+  // const $gravures = document.querySelector(".intro_gravures")
+  const typographyGravure = gsap.timeline()
+
+    .from(".garamond", {
+      opacity: 0,
+    })
+
+
+  ScrollTrigger.create({
+    trigger: ".garamond",
+    animation: typographyGravure,
+    start: "top 60%",
+    end: "bottom 50%",
+    scrub: 1,
+    // pin: ".engravings-gravures",
+    // pin: $intro,
+    // markers: true
+  });
+}
+
+
+
+
+
+
+const engravingTransitionAnimation = () => {
+
+  gsap.to(".engravings-gravures", {
+    duration: 3,
+    scrollTrigger: {
+      trigger: ".engravings",
+      start: "top 90%",
+      end: "top 0%",
+      // toggleClass: "white",
+      // markers: { fontSize: "25px", fontWeight: "bold" },
+      scrub: true,
+      pin: ".engravings-gravures",
+      pinSpacing: false, //set to true to see extra padding added
+    },
+  });
+
+}
+
+
+const typographyBgAnimation = () => {
+
+  const typographyBg = gsap.timeline()
+
+    .from(".typography_text", {
+      y: 500,
+      opacity: 0,
+    })
+
+
+  ScrollTrigger.create({
+    trigger: ".typography_intro",
+    animation: typographyBg,
+    start: "top 00%",
+    end: "bottom 20%",
+    scrub: 1,
+    pin: ".typography_intro",
+    pinSpacing: true,
+    // markers: true
+  });
+}
+
+
+
+const engravingsBgAnimation = () => {
+
+  const engravingsBg = gsap.timeline()
+
+    .from(".engravings_text", {
+      y: 500,
+      opacity: 0,
+    })
+
+
+  ScrollTrigger.create({
+    trigger: ".engravings_intro",
+    animation: engravingsBg,
+    start: "top 00%",
+    end: "bottom 20%",
+    scrub: 1,
+    pin: ".engravings_intro",
+    pinSpacing: true,
+    markers: true
+  });
+}
+
+const woodblockBgAnimation = () => {
+
+  const woodblockBg = gsap.timeline()
+
+    .from(".woodblock_text", {
+      y: 500,
+      opacity: 0,
+    })
+
+
+  ScrollTrigger.create({
+    trigger: ".woodblocks",
+    animation: woodblockBg,
+    start: "top 00%",
+    end: "bottom 20%",
+    scrub: 1,
+    pin: ".woodblocks",
+    pinSpacing: true,
+    // markers: true
+  });
+}
+
+
+
+
 const transitionSecondAnimation = () => {
 
-  // const $lettersSection = document.querySelector(".letters_section")
-  // const $gravures = document.querySelector(".intro_gravures")
   const lettersMovement = gsap.timeline()
 
     .from(".transition-1", {
@@ -261,21 +411,13 @@ const transitionSecondAnimation = () => {
       duration: 20,
       opacity: 0,
     })
-    // .from(".transition-7", {
-    //   y: 200,
-    //   opacity: 0,
-    //   duration: 20,
-    // })
+
     .from(".transition-3", {
       y: 100,
       duration: 20,
       opacity: 0,
     })
-    // .from(".transition-8", {
-    //   y: 400,
-    //   duration: 20,
-    //   opacity: 0,
-    // })
+
     .from(".transition-2", {
       y: 300,
       duration: 20,
@@ -304,7 +446,6 @@ const transitionSecondAnimation = () => {
   ScrollTrigger.create({
     trigger: ".woodblock_transition",
     animation: lettersMovement,
-    // start: "top 60%",
     start: "top 0%",
     end: "bottom 10%",
     scrub: 1,
@@ -320,7 +461,9 @@ const transitionSecondAnimation = () => {
 
 //typography game
 
-document.addEventListener("DOMContentLoaded", () => {
+// document.addEventListener("DOMContentLoaded", () => {
+
+const typographyGame = () => {
   // Map punch classes to letter spaces
   const letterMap = {
     "punch--A": "a_letter",
@@ -349,7 +492,8 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
   });
-});
+}
+// });
 
 document.addEventListener("DOMContentLoaded", () => {
   // Map each tool item class to its corresponding name
@@ -382,7 +526,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
 //printing game
 
-document.addEventListener("DOMContentLoaded", () => {  //removeDOM
+// document.addEventListener("DOMContentLoaded", () => {  //removeDOM
+const woodblockWorkshop = () => {
+
   const drawingCanvas = document.getElementById("drawingCanvas");
   const ctx = drawingCanvas.getContext("2d");
   const resultCanvas = document.getElementById("resultCanvas");
@@ -393,6 +539,41 @@ document.addEventListener("DOMContentLoaded", () => {  //removeDOM
 
   let isDrawing = false;
 
+
+
+
+  // remove functions - put in init
+
+  const startDrawing = (e) => {
+    e.preventDefault();
+    isDrawing = true;
+    ctx.beginPath();
+    ctx.moveTo(getX(e), getY(e));
+  }
+
+  const draw = (e) => {
+    if (!isDrawing) return;
+    e.preventDefault();
+    ctx.lineTo(getX(e), getY(e));
+    ctx.strokeStyle = "black"; // Customize line color
+    ctx.lineWidth = 2; // Customize line width
+    ctx.stroke();
+  }
+
+  const stopDrawing = () => {
+    // e.preventDefault();
+    isDrawing = false;
+    ctx.closePath();
+  }
+
+  const getX = (e) => {
+    return (e.clientX || (e.touches && e.touches[0].clientX)) - drawingCanvas.getBoundingClientRect().left;
+  }
+
+  const getY = (e) => {
+    return (e.clientY || (e.touches && e.touches[0].clientY)) - drawingCanvas.getBoundingClientRect().top;
+  }
+
   // Drawing functionality
   drawingCanvas.addEventListener("mousedown", startDrawing);
   drawingCanvas.addEventListener("mousemove", draw);
@@ -402,46 +583,15 @@ document.addEventListener("DOMContentLoaded", () => {  //removeDOM
   drawingCanvas.addEventListener("touchmove", draw);
   drawingCanvas.addEventListener("touchend", stopDrawing);
 
-
-  // remove functions - put in init
-
-  function startDrawing(e) {
-    e.preventDefault();
-    isDrawing = true;
-    ctx.beginPath();
-    ctx.moveTo(getX(e), getY(e));
-  }
-
-  function draw(e) {
-    if (!isDrawing) return;
-    e.preventDefault();
-    ctx.lineTo(getX(e), getY(e));
-    ctx.strokeStyle = "black"; // Customize line color
-    ctx.lineWidth = 2; // Customize line width
-    ctx.stroke();
-  }
-
-  function stopDrawing() {
-    e.preventDefault();
-    isDrawing = false;
-    ctx.closePath();
-  }
-
-  function getX(e) {
-    return (e.clientX || (e.touches && e.touches[0].clientX)) - drawingCanvas.getBoundingClientRect().left;
-  }
-
-  function getY(e) {
-    return (e.clientY || (e.touches && e.touches[0].clientY)) - drawingCanvas.getBoundingClientRect().top;
-  }
-
   // Handle "Done Drawing" button click
   doneButton.addEventListener("click", () => {
     // Show the Lottie animation container
     engravingAnimation.style.display = "block";
+    console.log('engraving video playing')
 
     // Load the Lottie animation
     const animation = lottie.loadAnimation({
+
       container: engravingAnimation, // Target the Lottie container
       renderer: "svg",
       loop: false,
@@ -451,8 +601,11 @@ document.addEventListener("DOMContentLoaded", () => {  //removeDOM
 
     // When the animation finishes
     animation.addEventListener("complete", () => {
+      drawingCanvas.style.display = "none"; // Hide animation
       engravingAnimation.style.display = "none"; // Hide animation
+      doneButton.style.display = "none";
       resultDiv.style.display = "block"; // Show the result
+      console.log('the picture was printed')
 
       // Mirror the drawing and display it on the papyrus
       const imageData = ctx.getImageData(0, 0, drawingCanvas.width, drawingCanvas.height);
@@ -462,7 +615,8 @@ document.addEventListener("DOMContentLoaded", () => {  //removeDOM
       resultCtx.restore();
     });
   });
-});
+}
+// });
 
 
 
@@ -480,6 +634,17 @@ const init = () => {
   transitionAnimation();
   introAnimation();
   introSecondAnimation();
+  typographyBgAnimation();
+
+  typographyAnimation();
+  typographySecondAnimation();
+
+  typographyGame();
+  // engravingMagicAnimation();
+  engravingTransitionAnimation();
+  engravingsBgAnimation();
+  woodblockBgAnimation();
+  woodblockWorkshop();
   transitionSecondAnimation();
 }
 init();
