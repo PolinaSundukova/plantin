1b8009bceb2c3d01325d15ef41d142eab11914cf
polina.sundukova <polina.sundukova@student.howest.be>
2025-01-24 14:19:25 +0100

    removed extra "}" from the end of js

--- src/main.js
+++ src/main.js
@@ -348,370 +348,369 @@ const engravingTransitionAnimation = () => {
     },
   });
 
-  // }
+}
 
 
-  const typographyBgAnimation = () => {
+const typographyBgAnimation = () => {
 
-    const typographyBg = gsap.timeline()
+  const typographyBg = gsap.timeline()
 
-      .from(".typography_text", {
-        y: 500,
-        opacity: 0,
-      })
-
-
-    ScrollTrigger.create({
-      trigger: ".typography_intro",
-      animation: typographyBg,
-      start: "top 00%",
-      end: "bottom 20%",
-      scrub: 1,
-      pin: ".typography_intro",
-      pinSpacing: true,
-      // markers: true
-    });
-  }
+    .from(".typography_text", {
+      y: 500,
+      opacity: 0,
+    })
 
 
-
-  const engravingsBgAnimation = () => {
-
-    const engravingsBg = gsap.timeline()
-
-      .from(".engravings_text", {
-        y: 500,
-        opacity: 0,
-      })
-
-
-    ScrollTrigger.create({
-      trigger: ".engravings_intro",
-      animation: engravingsBg,
-      start: "top 00%",
-      end: "bottom 20%",
-      scrub: 1,
-      pin: ".engravings_intro",
-      pinSpacing: true,
-      markers: true
-    });
-  }
-
-  const woodblockBgAnimation = () => {
-
-    const woodblockBg = gsap.timeline()
-
-      .from(".woodblock_text", {
-        y: 500,
-        opacity: 0,
-      })
-
-
-    ScrollTrigger.create({
-      trigger: ".woodblocks",
-      animation: woodblockBg,
-      start: "top 00%",
-      end: "bottom 20%",
-      scrub: 1,
-      pin: ".woodblocks",
-      pinSpacing: true,
-      // markers: true
-    });
-  }
+  ScrollTrigger.create({
+    trigger: ".typography_intro",
+    animation: typographyBg,
+    start: "top 00%",
+    end: "bottom 20%",
+    scrub: 1,
+    pin: ".typography_intro",
+    pinSpacing: true,
+    // markers: true
+  });
+}
 
 
 
+const engravingsBgAnimation = () => {
 
-  const transitionSecondAnimation = () => {
+  const engravingsBg = gsap.timeline()
 
-    const lettersMovement = gsap.timeline()
-
-      .from(".transition-1", {
-        y: 400,
-        duration: 20,
-        opacity: 0,
-      })
-
-      .from(".transition-3", {
-        y: 100,
-        duration: 20,
-        opacity: 0,
-      })
-
-      .from(".transition-2", {
-        y: 300,
-        duration: 20,
-        opacity: 0,
-      })
-
-      .from(".transition-6", {
-        y: 300,
-        duration: 20,
-        opacity: 0,
-      })
-      .from(".transition-5", {
-        y: 200,
-        duration: 20,
-        opacity: 0,
-      })
+    .from(".engravings_text", {
+      y: 500,
+      opacity: 0,
+    })
 
 
-      .from(".transition-4", {
-        y: 100,
-        opacity: 0,
-        duration: 20,
-      })
+  ScrollTrigger.create({
+    trigger: ".engravings_intro",
+    animation: engravingsBg,
+    start: "top 00%",
+    end: "bottom 20%",
+    scrub: 1,
+    pin: ".engravings_intro",
+    pinSpacing: true,
+    markers: true
+  });
+}
+
+const woodblockBgAnimation = () => {
+
+  const woodblockBg = gsap.timeline()
+
+    .from(".woodblock_text", {
+      y: 500,
+      opacity: 0,
+    })
 
 
-    ScrollTrigger.create({
-      trigger: ".woodblock_transition",
-      animation: lettersMovement,
-      start: "top 0%",
-      end: "bottom 10%",
-      scrub: 1,
-      pin: ".woodblock_transition",
-      // markers: true
-    });
-  }
+  ScrollTrigger.create({
+    trigger: ".woodblocks",
+    animation: woodblockBg,
+    start: "top 00%",
+    end: "bottom 20%",
+    scrub: 1,
+    pin: ".woodblocks",
+    pinSpacing: true,
+    // markers: true
+  });
+}
 
 
 
 
+const transitionSecondAnimation = () => {
+
+  const lettersMovement = gsap.timeline()
+
+    .from(".transition-1", {
+      y: 400,
+      duration: 20,
+      opacity: 0,
+    })
+
+    .from(".transition-3", {
+      y: 100,
+      duration: 20,
+      opacity: 0,
+    })
+
+    .from(".transition-2", {
+      y: 300,
+      duration: 20,
+      opacity: 0,
+    })
+
+    .from(".transition-6", {
+      y: 300,
+      duration: 20,
+      opacity: 0,
+    })
+    .from(".transition-5", {
+      y: 200,
+      duration: 20,
+      opacity: 0,
+    })
 
 
-  //typography game
+    .from(".transition-4", {
+      y: 100,
+      opacity: 0,
+      duration: 20,
+    })
 
-  // document.addEventListener("DOMContentLoaded", () => {
 
-  const typographyGame = () => {
-    // Map punch classes to letter spaces
-    const letterMap = {
-      "punch--A": "a_letter",
-      "punch--D": "d_letter",
-      "punch--G": "g_letter",
-      "punch--M": "m_letter",
-      "punch--N": "n_letter",
-      "punch--O": "o_letter",
-      "punch--R": "r_letter",
-    };
+  ScrollTrigger.create({
+    trigger: ".woodblock_transition",
+    animation: lettersMovement,
+    start: "top 0%",
+    end: "bottom 10%",
+    scrub: 1,
+    pin: ".woodblock_transition",
+    // markers: true
+  });
+}
 
-    document.querySelectorAll(".punch").forEach((punch) => {
-      punch.addEventListener("click", () => {
-        const punchClass = Array.from(punch.classList).find((cls) => cls.startsWith("punch--"));
-        const letterSpaceClass = letterMap[punchClass];
 
-        if (letterSpaceClass) {
-          // Find the first unfilled letter space and update it
-          const letterSpaces = document.querySelectorAll(`.${letterSpaceClass}`);
-          for (const letterSpace of letterSpaces) {
-            if (letterSpace.textContent === "?") {
-              letterSpace.textContent = punchClass.replace("punch--", "").toUpperCase();
-              break; // Stop after updating the first unfilled space
-            }
+
+
+
+
+//typography game
+
+// document.addEventListener("DOMContentLoaded", () => {
+
+const typographyGame = () => {
+  // Map punch classes to letter spaces
+  const letterMap = {
+    "punch--A": "a_letter",
+    "punch--D": "d_letter",
+    "punch--G": "g_letter",
+    "punch--M": "m_letter",
+    "punch--N": "n_letter",
+    "punch--O": "o_letter",
+    "punch--R": "r_letter",
+  };
+
+  document.querySelectorAll(".punch").forEach((punch) => {
+    punch.addEventListener("click", () => {
+      const punchClass = Array.from(punch.classList).find((cls) => cls.startsWith("punch--"));
+      const letterSpaceClass = letterMap[punchClass];
+
+      if (letterSpaceClass) {
+        // Find the first unfilled letter space and update it
+        const letterSpaces = document.querySelectorAll(`.${letterSpaceClass}`);
+        for (const letterSpace of letterSpaces) {
+          if (letterSpace.textContent === "?") {
+            letterSpace.textContent = punchClass.replace("punch--", "").toUpperCase();
+            break; // Stop after updating the first unfilled space
           }
         }
-      });
+      }
     });
+  });
+}
+// });
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Map each tool item class to its corresponding name
+  const toolNames = {
+    "tool_item--1": "Etching Needle",
+    "tool_item--2": "Dabber for applying",
+    "tool_item--3": "Scraper",
+    "tool_item--4": "Hammer",
+    "tool_item--5": "Scooper",
+    "tool_item--6": "Calliper Compasses",
+  };
+
+  // Add click event listeners to each tool item
+  document.querySelectorAll(".tool_item").forEach((toolItem) => {
+    toolItem.addEventListener("click", () => {
+      const toolClass = Array.from(toolItem.classList).find((cls) => cls.startsWith("tool_item--"));
+      if (toolClass) {
+        const toolName = toolNames[toolClass];
+        if (toolName) {
+          // Update the content of the paragraph with the tool name
+          document.querySelector(".tool_name").textContent = toolName;
+        }
+      }
+    });
+  });
+});
+
+
+
+
+
+
+//printing game
+
+// document.addEventListener("DOMContentLoaded", () => {  //removeDOM
+const woodblockWorkshop = () => {
+
+  const drawingCanvas = document.getElementById("drawingCanvas");
+  const ctx = drawingCanvas.getContext("2d");
+  const resultCanvas = document.getElementById("resultCanvas");
+  const resultCtx = resultCanvas.getContext("2d");
+  const doneButton = document.getElementById("doneButton");
+  const engravingAnimation = document.getElementById("engravingAnimation");
+  const resultDiv = document.querySelector(".result");
+  const animationCanvas = document.getElementById("anim-box")
+  let animationLottie;
+
+  let isDrawing = false;
+
+  console.log('MAIN workshop console test')
+
+
+  // remove functions - put in init
+
+  const startDrawing = (e) => {
+    e.preventDefault();
+    isDrawing = true;
+    ctx.beginPath();
+    ctx.moveTo(getX(e), getY(e));
   }
+
+  const draw = (e) => {
+    if (!isDrawing) return;
+    e.preventDefault();
+    ctx.lineTo(getX(e), getY(e));
+    ctx.strokeStyle = "black"; // Customize line color
+    ctx.lineWidth = 2; // Customize line width
+    ctx.stroke();
+  }
+
+  const stopDrawing = () => {
+    // e.preventDefault();
+    isDrawing = false;
+    ctx.closePath();
+  }
+
+  const getX = (e) => {
+    return (e.clientX || (e.touches && e.touches[0].clientX)) - drawingCanvas.getBoundingClientRect().left;
+  }
+
+  const getY = (e) => {
+    return (e.clientY || (e.touches && e.touches[0].clientY)) - drawingCanvas.getBoundingClientRect().top;
+  }
+
+  // Drawing functionality
+  drawingCanvas.addEventListener("mousedown", startDrawing);
+  drawingCanvas.addEventListener("mousemove", draw);
+  drawingCanvas.addEventListener("mouseup", stopDrawing);
+  drawingCanvas.addEventListener("mouseout", stopDrawing);
+  drawingCanvas.addEventListener("touchstart", startDrawing);
+  drawingCanvas.addEventListener("touchmove", draw);
+  drawingCanvas.addEventListener("touchend", stopDrawing);
+
+
+
+
+
+
+  ///// for option 3 (remove when not needed)
+
+  // doneButton.addEventListener("click", () => {
+  //   engravingAnimation.style.display = "block";
+
+  //   ///////////option 1
+  //   // Load the Lottie animation
+  //   // const animation = lottie.loadAnimation({
+  //   //   // container: engravingAnimation, // Target the Lottie container
+  //   //   // renderer: "svg",
+  //   //   loop: false,
+  //   //   autoplay: true,
+  //   //   path: "assets/engraving.json", // Lottie animation JSON file
+
+  //   /////////option 2
+  //   // const animationLottie = () => {
+  //   //   lottie.loadAnimation({
+  //   //     // container: engravingAnimation, // Target the Lottie container
+  //   //     // renderer: "svg",
+  //   //     canvas: document.querySelector("#anim-box"),
+  //   //     loop: false,
+  //   //     autoplay: true,
+  //   //     path: "assets/engraving.json", // Lottie animation JSON file
+  //   //   });
+  //   // }
+
+  //   animationLottie.addEventListener("complete", () => {
+  //     drawingCanvas.style.display = "none"; // Hide animation
+  //     engravingAnimation.style.display = "none"; // Hide animation
+  //     doneButton.style.display = "none";
+  //     resultDiv.style.display = "block"; // Show the result
+  //     console.log('the picture was printed')
+
+  //     // Mirror the drawing and display it on the papyrus
+  //     const imageData = ctx.getImageData(0, 0, drawingCanvas.width, drawingCanvas.height);
+  //     resultCtx.save();
+  //     resultCtx.scale(-1, 1); // Mirror horizontally
+  //     resultCtx.drawImage(drawingCanvas, -drawingCanvas.width, 0);
+  //     resultCtx.restore();
+  //   });
   // });
 
-  document.addEventListener("DOMContentLoaded", () => {
-    // Map each tool item class to its corresponding name
-    const toolNames = {
-      "tool_item--1": "Etching Needle",
-      "tool_item--2": "Dabber for applying",
-      "tool_item--3": "Scraper",
-      "tool_item--4": "Hammer",
-      "tool_item--5": "Scooper",
-      "tool_item--6": "Calliper Compasses",
-    };
 
-    // Add click event listeners to each tool item
-    document.querySelectorAll(".tool_item").forEach((toolItem) => {
-      toolItem.addEventListener("click", () => {
-        const toolClass = Array.from(toolItem.classList).find((cls) => cls.startsWith("tool_item--"));
-        if (toolClass) {
-          const toolName = toolNames[toolClass];
-          if (toolName) {
-            // Update the content of the paragraph with the tool name
-            document.querySelector(".tool_name").textContent = toolName;
-          }
-        }
-      });
+
+
+
+  //////////option 3
+
+
+
+  doneButton.addEventListener("click", () => {
+
+    animationCanvas.style.display = "block";
+
+    const animationLottie = new DotLottie({
+      autoplay: true,
+      loop: false,     // Disable looping
+      canvas: document.querySelector("#anim-box"), // Target the canvas container
+      src: "assets/engraving.json" // Path to the Lottie animation file
     });
+
+    animationLottie.addEventListener("complete", () => {
+      console.log("Animation complete!");
+    });
+
   });
 
 
-
-
-
-
-  //printing game
-
-  // document.addEventListener("DOMContentLoaded", () => {  //removeDOM
-  const woodblockWorkshop = () => {
-
-    const drawingCanvas = document.getElementById("drawingCanvas");
-    const ctx = drawingCanvas.getContext("2d");
-    const resultCanvas = document.getElementById("resultCanvas");
-    const resultCtx = resultCanvas.getContext("2d");
-    const doneButton = document.getElementById("doneButton");
-    const engravingAnimation = document.getElementById("engravingAnimation");
-    const resultDiv = document.querySelector(".result");
-    const animationCanvas = document.getElementById("anim-box")
-    let animationLottie;
-
-    let isDrawing = false;
-
-    console.log('MAIN workshop console test')
-
-
-    // remove functions - put in init
-
-    const startDrawing = (e) => {
-      e.preventDefault();
-      isDrawing = true;
-      ctx.beginPath();
-      ctx.moveTo(getX(e), getY(e));
-    }
-
-    const draw = (e) => {
-      if (!isDrawing) return;
-      e.preventDefault();
-      ctx.lineTo(getX(e), getY(e));
-      ctx.strokeStyle = "black"; // Customize line color
-      ctx.lineWidth = 2; // Customize line width
-      ctx.stroke();
-    }
-
-    const stopDrawing = () => {
-      // e.preventDefault();
-      isDrawing = false;
-      ctx.closePath();
-    }
-
-    const getX = (e) => {
-      return (e.clientX || (e.touches && e.touches[0].clientX)) - drawingCanvas.getBoundingClientRect().left;
-    }
-
-    const getY = (e) => {
-      return (e.clientY || (e.touches && e.touches[0].clientY)) - drawingCanvas.getBoundingClientRect().top;
-    }
-
-    // Drawing functionality
-    drawingCanvas.addEventListener("mousedown", startDrawing);
-    drawingCanvas.addEventListener("mousemove", draw);
-    drawingCanvas.addEventListener("mouseup", stopDrawing);
-    drawingCanvas.addEventListener("mouseout", stopDrawing);
-    drawingCanvas.addEventListener("touchstart", startDrawing);
-    drawingCanvas.addEventListener("touchmove", draw);
-    drawingCanvas.addEventListener("touchend", stopDrawing);
-
-
-
-
-
-
-    ///// for option 3 (remove when not needed)
-
-    // doneButton.addEventListener("click", () => {
-    //   engravingAnimation.style.display = "block";
-
-    //   ///////////option 1
-    //   // Load the Lottie animation
-    //   // const animation = lottie.loadAnimation({
-    //   //   // container: engravingAnimation, // Target the Lottie container
-    //   //   // renderer: "svg",
-    //   //   loop: false,
-    //   //   autoplay: true,
-    //   //   path: "assets/engraving.json", // Lottie animation JSON file
-
-    //   /////////option 2
-    //   // const animationLottie = () => {
-    //   //   lottie.loadAnimation({
-    //   //     // container: engravingAnimation, // Target the Lottie container
-    //   //     // renderer: "svg",
-    //   //     canvas: document.querySelector("#anim-box"),
-    //   //     loop: false,
-    //   //     autoplay: true,
-    //   //     path: "assets/engraving.json", // Lottie animation JSON file
-    //   //   });
-    //   // }
-
-    //   animationLottie.addEventListener("complete", () => {
-    //     drawingCanvas.style.display = "none"; // Hide animation
-    //     engravingAnimation.style.display = "none"; // Hide animation
-    //     doneButton.style.display = "none";
-    //     resultDiv.style.display = "block"; // Show the result
-    //     console.log('the picture was printed')
-
-    //     // Mirror the drawing and display it on the papyrus
-    //     const imageData = ctx.getImageData(0, 0, drawingCanvas.width, drawingCanvas.height);
-    //     resultCtx.save();
-    //     resultCtx.scale(-1, 1); // Mirror horizontally
-    //     resultCtx.drawImage(drawingCanvas, -drawingCanvas.width, 0);
-    //     resultCtx.restore();
-    //   });
-    // });
-
-
-
-
-
-    //////////option 3
-
-
-
-    doneButton.addEventListener("click", () => {
-
-      animationCanvas.style.display = "block";
-
-      const animationLottie = new DotLottie({
-        autoplay: true,
-        loop: false,     // Disable looping
-        canvas: document.querySelector("#anim-box"), // Target the canvas container
-        src: "assets/engraving.json" // Path to the Lottie animation file
-      });
-
-      animationLottie.addEventListener("complete", () => {
-        console.log("Animation complete!");
-      });
-
-    });
-
-
-  }
-  // });
-
-
-
-
-
-
-
-
-
-
-  const init = () => {
-    gsap.registerPlugin(ScrollTrigger);
-    console.log('init console test')
-    // gsap.registerPlugin(MotionPathPlugin);
-    timelineScrollAnimation();
-    transitionAnimation();
-    introAnimation();
-    introSecondAnimation();
-    typographyBgAnimation();
-
-    typographyAnimation();
-    typographySecondAnimation();
-
-    typographyGame();
-    // engravingMagicAnimation();
-    engravingTransitionAnimation();
-    engravingsBgAnimation();
-    woodblockBgAnimation();
-    woodblockWorkshop();
-    transitionSecondAnimation();
-  }
-  init();
 }
+// });
+
+
+
+
+
+
+
+
+
+
+const init = () => {
+  gsap.registerPlugin(ScrollTrigger);
+  console.log('init console test')
+  // gsap.registerPlugin(MotionPathPlugin);
+  timelineScrollAnimation();
+  transitionAnimation();
+  introAnimation();
+  introSecondAnimation();
+  typographyBgAnimation();
+
+  typographyAnimation();
+  typographySecondAnimation();
+
+  typographyGame();
+  engravingTransitionAnimation();
+  engravingsBgAnimation();
+  woodblockBgAnimation();
+  // woodblockWorkshop();
+  transitionSecondAnimation();
+}
+init();
+
